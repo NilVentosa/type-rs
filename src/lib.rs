9eded815101364dac498cc1479aa30c8528f58dd
nilventosa <ventosa@gmail.com>
2023-01-03 11:47:31 +0100

    Print random rust code line

--- src/lib.rs
+++ src/lib.rs
@@ -4,43 +4,36 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::env;
 use std::{
+    error::Error,
     fs::File,
     io::{BufRead, BufReader},
 };
 
-pub fn run() -> Result<(), String> {
-    let path = get_random_file_path().unwrap();
-    let line = get_random_line_from_file(&path).unwrap();
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let line = get_random_line()?;
     let term = Term::stdout();
-    term.write_str(&format!("{}", line)).unwrap();
-    let thing = term.read_char().unwrap();
-    term.clear_line().unwrap();
-    term.write_line(&format!("{}", thing)).unwrap();
+    term.write_str(&line)?;
     Ok(())
 }
 
-fn get_random_file_path() -> Option<String> {
-    if let Ok(home) = env::var("HOME") {
-        let entries: Vec<String> = glob("{home}/.cargo/registry/src/**/*.rs")
-            .unwrap()
-            .map(|p| p.unwrap().display().to_string())
-            .collect();
-        Some(entries.choose(&mut thread_rng()).unwrap().to_string())
-    } else {
-        None
-    }
+fn get_random_file_path() -> Result<String, Box<dyn Error>> {
+    let home = env::var("HOME")?;
+    let entries: Vec<String> = glob(&format!("{home}/.cargo/registry/src/**/*.rs"))?
+        .map(|p| p.unwrap().display().to_string())
+        .collect();
+    println!("{}", entries.len());
+    return Ok(entries.choose(&mut thread_rng()).unwrap().to_string());
 }
 
-fn get_random_line_from_file(file_path: &str) -> Option<String> {
-    if let Ok(file) = File::open(file_path) {
-        let lines: Vec<String> = BufReader::new(file)
-            .lines()
-            .filter_map(|l| l.ok())
-            .filter(|l| !l.contains('/'))
-            .map(|l| l.trim().to_string())
-            .filter(|l| l.len() > 10)
-            .collect();
-        return Some(lines.choose(&mut thread_rng()).unwrap().to_string());
-    }
-    None
+fn get_random_line() -> Result<String, Box<dyn Error>> {
+    let path = get_random_file_path()?;
+    let file = File::open(path)?;
+    let lines: Vec<String> = BufReader::new(file)
+        .lines()
+        .filter_map(|l| l.ok())
+        .filter(|l| !l.contains('/'))
+        .map(|l| l.trim().to_string())
+        .filter(|l| l.len() > 10)
+        .collect();
+    Ok(lines.choose(&mut thread_rng()).unwrap().to_string())
 }

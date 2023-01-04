043abf17e12046cf93cc7858162b7d7ae581a351
nilventosa <ventosa@gmail.com>
2023-01-04 08:19:20 +0100

    Color changes as you type

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 This should be a cli to practice touch typing using lines of rust code. Maybe eventually to be extended to other languages.
 
 ## TODO
-[x] Open random file and print random line  
-[] Change color as you type
+- [x] Open random file and print random line  
+- [x] Change color as you type
+- [ ] Results after each line
+- [ ] Results at the end
+- [ ] Choose line or block
+- [ ] Separate the code fetching to a different crate
+- [ ] Add clap
+- [ ] Add instructions
+- [ ] ...
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,66 @@
+use console::style;
+use console::Key;
 use console::Term;
 use glob::glob;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::env;
+use std::fmt;
 use std::{
     error::Error,
     fs::File,
     io::{BufRead, BufReader},
 };
 
+#[derive(Debug)]
+struct Errrr(String);
+
+impl fmt::Display for Errrr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There is an error: {}", self.0)
+    }
+}
+
+impl Error for Errrr {}
+
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let line = get_random_line()?;
     let term = Term::stdout();
-    term.write_str(&line)?;
+    term.clear_screen()?;
+    line_loop(&term)?;
+    Ok(())
+}
+
+fn character_loop(term: &Term, c: char) -> Result<(), Box<dyn Error>> {
+    loop {
+        let input = term.read_key()?;
+        if input == Key::Escape {
+            return Err(Box::new(Errrr("Oops".into())));
+        } else if input == Key::Char(c) {
+            term.show_cursor()?;
+            term.write_str(&format!("{}", style(c).green().bold()))?;
+            break;
+        } else {
+            term.hide_cursor()?;
+            term.write_str(&format!("{}", style(c).red().bold()))?;
+            term.move_cursor_left(1)?;
+        }
+    }
+    Ok(())
+}
+
+fn line_loop(term: &Term) -> Result<(), Box<dyn Error>> {
+    'outer: loop {
+        let line = get_random_line()?;
+        term.write_str(&line)?;
+        let characters: Vec<char> = line.chars().collect();
+        term.move_cursor_left(characters.len())?;
+        for c in characters {
+            if let Err(_) = character_loop(&term, c) {
+                break 'outer;
+            }
+        }
+        term.write_line("")?;
+    }
     Ok(())
 }
 
@@ -21,7 +69,6 @@ fn get_random_file_path() -> Result<String, Box<dyn Error>> {
     let entries: Vec<String> = glob(&format!("{home}/.cargo/registry/src/**/*.rs"))?
         .map(|p| p.unwrap().display().to_string())
         .collect();
-    println!("{}", entries.len());
     return Ok(entries.choose(&mut thread_rng()).unwrap().to_string());
 }
 

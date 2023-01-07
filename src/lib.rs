4ffb760dffe4da94f5f841c92f472f43dc585910
nilventosa <ventosa@gmail.com>
2023-01-07 08:15:13 +0100

    Move the line fetching to a different crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
+use code_lines::{get_random_line, Language::Rust, LineConfig};
 use console::style;
 use console::Key;
 use console::Term;
-use glob::glob;
-use rand::seq::SliceRandom;
-use rand::thread_rng;
-use std::env;
+use std::error::Error;
 use std::time::SystemTime;
-use std::{
-    error::Error,
-    fs::File,
-    io::{BufRead, BufReader},
-};
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let term = Term::stdout();
@@ -20,7 +13,8 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     ))?;
     let mut code_lines: Vec<CodeLine> = vec![];
     loop {
-        let code_line = CodeLine::new(get_random_line().unwrap()).play(&term);
+        let code_line =
+            CodeLine::new(get_random_line(&LineConfig { language: Rust }).unwrap()).play(&term);
         code_lines.push(code_line);
 
         if code_lines.last().is_some() && !code_lines.last().unwrap().completed {
@@ -134,25 +128,4 @@ fn print_total_results(code_lines: &Vec<CodeLine>, term: &Term) {
         .yellow()
     ))
     .unwrap();
-}
-
-fn get_random_file_path() -> Result<String, Box<dyn Error>> {
-    let home = env::var("HOME")?;
-    let entries: Vec<String> = glob(&format!("{home}/.cargo/registry/src/**/*.rs"))?
-        .map(|p| p.unwrap().display().to_string())
-        .collect();
-    return Ok(entries.choose(&mut thread_rng()).unwrap().to_string());
-}
-
-fn get_random_line() -> Result<String, Box<dyn Error>> {
-    let path = get_random_file_path()?;
-    let file = File::open(path)?;
-    let lines: Vec<String> = BufReader::new(file)
-        .lines()
-        .filter_map(|l| l.ok())
-        .filter(|l| !l.contains('/'))
-        .map(|l| l.trim().to_string())
-        .filter(|l| l.len() > 10)
-        .collect();
-    Ok(lines.choose(&mut thread_rng()).unwrap().to_string())
 }
